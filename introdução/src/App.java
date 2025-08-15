e23793acec3c28d94391f28f63c564d4d5de63db
PauloNeto-cloud <pbaneto13@gmail.com>
2025-08-15 18:11:43 +0000

    ntrodução

--- introdução/src/App.java
+++ introdução/src/App.java
@@ -1,5 +1,7 @@
 public class App {
     public static void main(String[] args) throws Exception {
         pessoas pi = new pessoas("paulo neto", "123.456.789-00");
+        pi.exibirDados();
+        
     }
 }
