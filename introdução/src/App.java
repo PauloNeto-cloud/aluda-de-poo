7d2a3d17d2c867017eac7cda497fe864239d53d7
PauloNeto-cloud <pbaneto13@gmail.com>
2025-08-17 02:58:36 +0000

    introdução

--- introdução/src/App.java
+++ introdução/src/App.java
@@ -1,7 +1,22 @@
+// Classe principal do programa
+// É aqui que o programa começa (main)
 public class App {
-    public static void main(String[] args) throws Exception {
-        pessoas pi = new pessoas("paulo neto", "123.456.789-00");
-        pi.exibirDados();
-        
+    // Método principal que roda quando executamos o programa
+    public static void main(String[] args) {
+        // Criando um objeto da classe Pessoa
+        // Chamamos o construtor e passamos nome e CPF
+        pessoas p1 = new pessoas("João Silva", "123.456.789-00");
+
+        // Criando outro objeto Pessoa
+        pessoas p2 = new pessoas("Maria Oliveira", "987.654.321-00");
+
+        // Chamando o método exibirDados() para mostrar no terminal
+        p1.exibirDados();
+
+        // Linha em branco só para separar as saídas
+        System.out.println();
+
+        // Exibindo dados da segunda pessoa
+        p2.exibirDados();
     }
 }
--- introdução/src/pessoas.java
+++ introdução/src/pessoas.java
@@ -1,26 +1,33 @@
-
+// Definição da classe Pessoa
 public class pessoas {
+    // Atributos privados (encapsulamento)
+    // 'private' significa que só podem ser acessados dentro desta classe
     private String nome;
     private String cpf;
 
-    //construtor
-    public pessoas(String nome, String cpf){
+    // Construtor da classe
+    // É chamado automaticamente quando criamos uma nova Pessoa
+    // Recebe dois parâmetros: nome e cpf
+    public pessoas(String nome, String cpf) {
+        // 'this.nome' se refere ao atributo da classe
+        // 'nome' (sem this) é o parâmetro recebido
         this.nome = nome;
-        this.cpf = cpf;   
+        this.cpf = cpf;
     }
 
-    //getters
-    public String getNome(){
+    // Getter (serve para pegar o valor do atributo 'nome')
+    public String getNome() {
         return nome;
     }
 
-    public String getCpf(){
+    // Getter (serve para pegar o valor do atributo 'cpf')
+    public String getCpf() {
         return cpf;
     }
 
-    //metodo para exebir
-    public void exibirDados(){
-        System.out.println("nome: " + nome);
+    // Método para exibir os dados no terminal
+    public void exibirDados() {
+        System.out.println("Nome: " + nome);
         System.out.println("CPF: " + cpf);
     }
 }
